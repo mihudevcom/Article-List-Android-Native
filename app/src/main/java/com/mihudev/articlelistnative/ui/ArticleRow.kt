ce3ceb74e8cf71d8e158d97e348fbbe234f29fb8
Mihai Boisteanu <mihai.boisteanu@gmail.com>
2025-01-24 11:41:43 +0200

    Add animations for like button and screen transitions

--- app/src/main/java/com/mihudev/articlelistnative/ui/ArticleRow.kt
+++ app/src/main/java/com/mihudev/articlelistnative/ui/ArticleRow.kt
@@ -1,5 +1,6 @@
 package com.mihudev.articlelistnative.ui
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mihudev.articlelistnative.model.Article
@@ -31,8 +34,17 @@ fun ArticleRow(
             .clickable { onArticleClick(article.id) },
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
+
+        val scale by animateFloatAsState(
+            targetValue = if (article.liked) 1.2f else 1.0f
+        )
+
+
         Text(text = article.title)
-        IconButton(onClick = { onLikeToggle(article.id) }) {
+        IconButton(
+            onClick = { onLikeToggle(article.id) },
+            modifier = Modifier.graphicsLayer(scaleX = scale, scaleY = scale)
+        ) {
             Icon(
                 imageVector = if (article.liked) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
                 contentDescription = "Like Button"
