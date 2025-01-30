7c49bff9187ec5117b31d83d44a2c5d541dfd0e6
Mihai Boisteanu <mihai.boisteanu@gmail.com>
2025-01-30 10:21:01 +0200

    Fixed loading Spinner size and position

--- app/src/main/java/com/mihudev/articlelistnative/ui/ArticleListScreen.kt
+++ app/src/main/java/com/mihudev/articlelistnative/ui/ArticleListScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -52,12 +53,16 @@ fun ArticleListScreen(
                 )
             }
             item {
-                if (viewModel.isLoading) {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                    )
+                if (viewModel.isLoading && errorMessage == null) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .padding(16.dp)
+                        )
+                    }
                 }
             }
         }
